d09d95dda8c1da192ce52fc35ad4c22e9491da8c
Mateus Moura Domingos <mateus.md1902@gmail.com>
2022-12-17 18:18:48 -0300

    terminado versão mobile do projeto

--- assets/script.js
+++ assets/script.js
@@ -22,12 +22,13 @@ const contentsOfContainers = (data) => {
         classes[1][i].textContent = `${data[i].timeframes.weekly.current}hrs`
         classes[2][i].textContent = `Last Week - ${data[i].timeframes.weekly.previous}hrs`
     }
-    
-    ListenConteiners(data, classes)
+
+    listenConteiners(data, classes)
+    colorSections()
 }
 
 // function where it takes the id of the referenced container and adds an event listener for the containers
-const ListenConteiners = (data, classes) => {
+const listenConteiners = (data, classes) => {
     const dayly = document.getElementById('dayly')
     const weekly = document.getElementById('weekly')
     const monthly = document.getElementById('monthly')
@@ -36,13 +37,13 @@ const ListenConteiners = (data, classes) => {
 
     for (let container of listenersContainers) {
         container.addEventListener('click', () => {
-            changeHours(data, container, classes)
+            changeHours(data, container, classes, listenersContainers)
         })
     }
 }
 
 // function where to change the content of the container according to the container that was clicked
-const changeHours = (data, container, classes) => {
+const changeHours = (data, container, classes, listenersContainers) => {
     const dailyContainer = container.textContent == 'Daily'
     const weeklyContainer = container.textContent == 'Weekly'
     const monthlyContainer = container.textContent == 'Monthly'
@@ -64,6 +65,47 @@ const changeHours = (data, container, classes) => {
             classes[0][i].textContent = data[i].title
             classes[1][i].textContent = `${data[i].timeframes.monthly.current}hrs`
             classes[2][i].textContent = `Last Week - ${data[i].timeframes.monthly.previous}hrs`
+        }
+    }
+
+    if (container.className == 'li-color-blue') {
+        container.className = 'li-color-white'
+
+        for (let i = 0; i < listenersContainers.length; i++) {
+            if (container.textContent != listenersContainers[i].textContent) {
+                listenersContainers[i].className = 'li-color-blue'
+            }
+        }
+    }
+}
+
+const colorSections = () => {
+    const section = document.getElementsByClassName('section-container');
+    const titles = document.getElementsByClassName('title');
+
+
+    for(let i = 0; i < titles.length; i++) {
+        switch (titles[i].textContent) {
+            case 'Work':
+                section[i].className = 'section-container work-section'
+                break;
+            case 'Play':
+                section[i].className = 'section-container play-section'
+                break;
+            case 'Study':
+                section[i].className = 'section-container study-section'
+                break;
+            case 'Exercise':
+                section[i].className = 'section-container exercise-section'
+                break;
+            case 'Social':
+                section[i].className = 'section-container social-section'
+                break;
+            case 'Self Care':
+                section[i].className = 'section-container self-care-section'
+                break;
+            default:
+                break;
         }
     }
 }
